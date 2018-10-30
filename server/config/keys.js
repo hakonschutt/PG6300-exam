2318f9a46a8c2a3eb53c249ca9bda9cc46466e76
Håkon Wang Schutt <hakon@schutt.no>
2018-10-30 18:47:25 +0100

    added nginx and finished docker-compose config

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,11 +1,6 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  globalApiKey: process.env.GLOBAL_API_KEY,
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
-  postgresUser: process.env.PGUSER,
-  postgresHost: process.env.PGHOST,
-  postgresDatabase: process.env.PGDATABASE,
-  postgresPassword: process.env.PGPASSWORD,
-  postgresPort: process.env.PGPORT
+  apiKey: process.env.API_KEY,
+  redisUri: process.env.REDIS_URL,
+  postgresUri: process.env.DATABASE_URL
 };
