d97eeb444198350d4046e424134e19e37f086879
Håkon Wang Schutt <hakon@schutt.no>
2018-10-30 15:35:21 +0100

    finished script for client and root package

--- frontend/package-scripts.js
+++ frontend/package-scripts.js
@@ -7,15 +7,40 @@ module.exports = {
   scripts: {
     fresh: {
       description: "clean the project before delievery",
-      default: rimraf('public'),
+      default: concurrent.nps('fresh.code', 'fresh.dependencies'),
+      code: rimraf('public'),
+      dependencies: rimraf('node_modules')
+    },
+    check: {
+      description: 'Checks project for errors and warnings',
+      default: concurrent.nps('check.lint', 'check.test'),
+      lint: 'eslint .',
+      test: 'jest'
+    },
+    prepublish: {
+      description: 'Executes checks before publishing',
+      default: series.nps('check', 'fresh')
     },
     build: {
       description: 'Build project',
-      default: series(rimraf('public'), 'NODE_ENV=production webpack --progress --config webpack/webpack.prod.config.js'),
+      default: series.nps('fresh.code', 'build.code'),
+      code: 'NODE_ENV=production webpack --progress --config webpack/webpack.prod.config.js'
+    },
+    test: {
+      description: 'Run tests',
+      default: 'jest -- --coverage',
+      watch: {
+        description: 'Run tests in watch mode',
+        default: 'jest -- --watch'
+      }
     },
     dev: {
       description: 'Run dev server',
-      default: 'webpack-dev-server --progress --config webpack/webpack.dev.config.js'
+      default: 'webpack-dev-server --progress --config webpack/webpack.dev.config.js',
+      watch: {
+        description: 'Run dev server in watch mode with tests',
+        default: concurrent.nps('dev', 'dev.test.watch')
+      }
     }
   }
 };
--- package-scripts.js
+++ package-scripts.js
@@ -5,23 +5,30 @@ const concurrent = npsUtils.concurrent;
 
 module.exports = {
   scripts: {
-    fresh: {
+    publish: {
       description: "clean the project before delievery",
-      default: series(
-        rimraf("server/node_modules"),
-        rimraf("frontend/node_modules"),
-        rimraf("node_modules")
-      )
+      default: concurrent.nps('fresh.frontend', 'fresh.server', 'fresh.parent'),
+      frontend: 'npm run clean --prefix frontend',
+      server: 'npm run clean --prefix server',
+      parent: rimraf('node_modules')
     },
-    start: {
-      description: "Run production server",
-      default: "node index.js"
+    setup: {
+      description: "Install all dependencies and run build setup",
+      default: concurrent.nps('setup.frontend', 'setup.server'),
+      frontend: 'npm install --prefix frontend --silent',
+      server: 'npm install --prefix server --silent'
     },
     test: {
       description: "Run all tests",
-      default: concurrent.nps("frontend", "server"),
+      default: concurrent.nps("test.frontend", "test.server"),
       frontend: "npm run test --prefix frontend",
       server: "npm run test --prefix server"
+    },
+    start: {
+      description: "Run production server",
+      default: series.nps('start.build', 'start.server'),
+      build: 'npm run build --prefix frontend',
+      server: 'node index.js'
     }
   }
 };
