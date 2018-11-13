7cb1e32935ba2da6abe895a17305fddd6997f731
Håkon Wang Schutt <hakon@schutt.no>
2018-11-13 23:46:45 +0100

    finished crud for quiz

--- server/database/models/Quiz.js
+++ server/database/models/Quiz.js
@@ -1,8 +1,91 @@
+const Database = require('../Database');
+const queries = require('../queries');
+
 class Quiz {
-  constructor({ id = null, title, numberOfPlayers = 4 }) {
-    this.id = id;
+  constructor({ quizId = null, title, numberOfPlayers = 4 }) {
+    this.quizId = quizId;
     this.title = title;
     this.numberOfPlayers = numberOfPlayers;
+  }
+
+  async save() {
+    try {
+      const { rows } = await Database.query(
+        queries.insertNewQuiz,
+        [this.title, this.numberOfPlayers]
+      );
+
+      this.quizId = rows[0].quizId;
+      return this;
+    }
+    catch (err) {
+      throw new Error('Could not save the quiz');
+    }
+  }
+
+  async update({ title = null, numberOfPlayers = null }) {
+    if (!this.quizId) {
+      throw new Error('You need to save the quiz before it can be updated');
+    }
+
+    if (!title && !numberOfPlayers) {
+      throw new Error('You need to include a title or number of players');
+    }
+
+    try {
+      if (title) {
+        this.title = title;
+      }
+
+      if (numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+      }
+
+      await Database.query(
+        queries.updateQuiz,
+        [this.quizId, this.title, this.numberOfPlayers]
+      );
+
+      return this;
+    }
+    catch (err) {
+      throw new Error('Could not update user');
+    }
+  }
+
+  static async findById(id) {
+    try {
+      const { rows } = await Database.query(queries.findQuizById, [id]);
+
+      if (rows.length < 1) throw new Error();
+
+      return new Quiz(rows[0]);
+    }
+    catch (err) {
+      throw new Error('Could not find the quiz');
+    }
+  }
+
+  static async findAll() {
+    try {
+      const { rows } = await Database.query(queries.findAllQuizzes);
+      return rows.map(row => new Quiz(row));
+    }
+    catch (err) {
+      throw new Error('An issu occured. Try again later');
+    }
+  }
+
+  static async deleteById(id) {
+    try {
+      const { rows } = await Database.query(queries.deleteQuizById, [id]);
+      await Database.query(queries.deleteQuestionByQuizId, [id]);
+
+      return rows[0].quizId === id;
+    }
+    catch (err) {
+      throw new Error('Could not delete quiz');
+    }
   }
 }
 
--- server/database/queries.js
+++ server/database/queries.js
@@ -12,6 +12,24 @@ module.exports = {
       PRIMARY KEY (user_id)
     )
   `,
+  insertNewUser: `
+    INSERT INTO users (name, email, password) VALUES ($1, $2, $3) returning user_id
+  `,
+  findUserById: `
+    SELECT * FROM users WHERE user_id = $1
+  `,
+  findUserByEmail: `
+    SELECT * FROM users WHERE email = $1
+  `,
+  findAllUsers: `
+    SELECT * FROM users
+  `,
+  updateUser: `
+    UPDATE users SET name = $2, email = $3, password = $4 WHERE user_id = $1 returning user_id
+  `,
+  deleteUserById: `
+    DELETE FROM users WHERE user_id = $1 returning user_id
+  `,
   createQuizTable: `
     CREATE TABLE IF NOT EXISTS quizzes (
       quiz_id             uuid                DEFAULT       uuid_generate_v4(),
@@ -20,6 +38,21 @@ module.exports = {
 
       PRIMARY KEY (quiz_id)
     )
+  `,
+  insertNewQuiz: `
+    INSERT INTO quizzes (title, number_of_players) VALUES ($1, $2) returning quiz_id
+  `,
+  findQuizById: `
+    SELECT * FROM quizzes WHERE quiz_id = $1
+  `,
+  findAllQuizzes: `
+    SELECT * FROM quizzes
+  `,
+  updateQuiz: `
+    UPDATE quizzes SET title = $2, number_of_players = $3 WHERE quiz_id = $1
+  `,
+  deleteQuizById: `
+    DELETE FROM quizzes WHERE quiz_id = $1 returning quiz_id
   `,
   createQuestionTable: `
     CREATE TABLE IF NOT EXISTS questions (
@@ -34,22 +67,34 @@ module.exports = {
       FOREIGN KEY (quiz_id) REFERENCES quizzes(quiz_id)
     )
   `,
-  insertNewUser: `
-    INSERT INTO users (name, email, password) VALUES ($1, $2, $3) returning user_id
+  insertNewQuestion: `
+    INSERT INTO questions (
+      question,
+      answers,
+      correct_index,
+      order_in_quiz,
+      quiz_id
+    ) VALUES ($1, $2, $3, $4, $5) returning question_id
   `,
-  findUserById: `
-    SELECT * FROM users WHERE user_id = $1
+  findQuestionById: `
+    SELECT * FROM questions WHERE question_id = $1
   `,
-  findUserByEmail: `
-    SELECT * FROM users WHERE email = $1
+  findAllQuestions: `
+    SELECT * FROM questions
   `,
-  findAllUsers: `
-    SELECT * FROM users
+  updateQuestion: `
+    UPDATE questions
+    SET question = $2,
+        answers = $3,
+        correct_index = $4,
+        order_in_quiz = $5,
+        quiz_id = $6
+    WHERE question_id = $1
   `,
-  updateUser: `
-    UPDATE users SET name = $2, email = $3, password = $4 WHERE id = $1 returning name
+  deleteQuestionById: `
+    DELETE FROM questions WHERE question_id = $1 returning question_id
   `,
-  deleteUserById: `
-    DELETE FROM users WHERE id = $1 returning id
+  deleteQuestionByQuizId: `
+    DELETE FROM questions WHERE quiz_id = $1 returning quiz_id
   `
 };
