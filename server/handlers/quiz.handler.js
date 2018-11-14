c7023c3062e493f62d04220c64b68a341d90638c
Håkon Wang Schutt <hakon@schutt.no>
2018-11-14 16:07:47 +0100

    finished quiz handler

--- server/handlers/quiz.handler.js
+++ server/handlers/quiz.handler.js
@@ -18,15 +18,70 @@ exports.getQuizzes = async (req, res) => {
 
 exports.createQuiz = async (req, res) => {
   try {
-
+    const quiz = await new Quiz(req.body).save();
+    res.status(201).send(quiz);
   }
   catch (err) {
     res.status(400).send();
   }
 };
 
-exports.updateQuiz = async (req, res) => {};
-exports.deleteQuiz = async (req, res) => {};
-exports.createQuizQuestion = async (req, res) => {};
-exports.updateQuizQuestion = async (req, res) => {};
-exports.deleteQuizQuestion = async (req, res) => {};
+exports.updateQuiz = async (req, res) => {
+  try {
+    let quiz = await Quiz.findById(req.params.id);
+    quiz = await quiz.update(req.body);
+
+    res.status(200).send(quiz);
+  }
+  catch (err) {
+    res.status(400).send();
+  }
+};
+
+exports.deleteQuiz = async (req, res) => {
+  try {
+    const deleted = await Quiz.deleteById(req.params.id);
+
+    if (deleted) {
+      res.status(204).send();
+    } else {
+      res.status(400).send();
+    }
+  }
+  catch (err) {
+    res.status(400).send();
+  }
+};
+
+exports.createQuizQuestion = async (req, res) => {
+  try {
+    const question = await new Question({ quizId: req.params.id, ...req.body });
+
+    res.status(201).send(question);
+  }
+  catch (err) {
+    res.status(400).send();
+  }
+};
+
+exports.updateQuizQuestion = async (req, res) => {
+  try {
+    const question = await Question.findById(req.params.questionId);
+    question.update(req.body);
+
+    res.status(200).send();
+  }
+  catch (err) {
+    res.status(400).send();
+  }
+};
+exports.deleteQuizQuestion = async (req, res) => {
+  try {
+    await Question.deleteById(req.params.questionId);
+    
+    res.status(200).send();
+  }
+  catch (err) {
+    res.status(400).send();
+  }
+};
