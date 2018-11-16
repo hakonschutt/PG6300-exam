9b644428b0daa894a303c6315832b699b5808818
Håkon Wang Schutt <hakon@schutt.no>
2018-11-16 01:26:55 +0100

    finished landing page

--- frontend/src/assets/images/index.js
+++ frontend/src/assets/images/index.js
@@ -1,1 +1,2 @@
 export { default as Logo } from '@assets/images/icons/Logo.jsx';
+export { default as Questions } from '@assets/images/icons/Questions.jsx';
--- frontend/src/containers/LandingPage.js
+++ frontend/src/containers/LandingPage.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Questions } from '@assets/images';
 
 const LandingPage = () => {
 	return (
-		<div className="landing-page wrap hpad">
-			<div className="options-list">
-				<h1>LandingPage</h1>
+		<div className="landing-page">
+			<div className="top-banner">
+				<div className="wrap hpad">
+					<div className="left-info">
+						<h1>Questions!</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat
+						</p>
+						<div className="button-wrap">
+							<Link to="/signup">
+								<span>Sign up now!</span>
+							</Link>
+						</div>
+					</div>
+					<div className="right-image">
+						<div className="image-wrap">
+							<Questions />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="info-center">
+				<div className="contain hpad">
+					<h2>How are we?</h2>
+					<p>
+						At vero eos et accusamus et iusto odio dignissimos ducimus qui
+						blanditiis praesentium voluptatum deleniti atque corrupti quos
+						dolores et quas molestias excepturi sint occaecati cupiditate non
+						provident, similique sunt in culpa qui officia deserunt mollitia
+						animi, id est laborum et dolorum fuga.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
